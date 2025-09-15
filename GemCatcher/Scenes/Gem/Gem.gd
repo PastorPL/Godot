659a8dc9a2e0ec065f898f3c157c331e2c3d78ac
Dreamteam <dreamteam@Mac-mini-Dreamteam.local>
2025-09-15 21:27:24 +0200

    Add padles

--- GemCatcher/Scenes/Gem/Gem.gd
+++ GemCatcher/Scenes/Gem/Gem.gd
@@ -1,10 +1,17 @@
 extends Area2D
 
+const SPEED: float = 100.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y += 100*delta
+	position.y += SPEED*delta
+	
+	if position.y > get_viewport_rect().end.y: 
+		print("Gem is off")
+		set_process(false)
+		queue_free()
---
+++ GemCatcher/Scenes/Paddle/Paddle.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_left") and position.x - 3 > get_viewport_rect().position.x:
+		position.x -= 200*delta 
+	else: if Input.is_action_pressed("move_right"):
+		position.x +=3	
+		
