3dc10cfb058875e8b7a6c574cf46933ef5a07c8b
Dreamteam <dreamteam@Mac-mini-Dreamteam.local>
2025-09-15 21:28:38 +0200

    Changes

--- GemCatcher/Scenes/Paddle/Paddle.gd
+++ GemCatcher/Scenes/Paddle/Paddle.gd
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_left") and position.x - 3 > get_viewport_rect().position.x:
 		position.x -= 200*delta 
 	else: if Input.is_action_pressed("move_right"):
-		position.x +=3	
+		position.x += 200*delta	
 		
